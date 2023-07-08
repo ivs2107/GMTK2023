db887a48b6b59ad403c2e8c423a6579622c29700
Lorenzo Di Benedetto <lorenzo.dibenedetto@he-arc.ch>
2023-07-08 17:03:34 +0200

    Ajout Sliders et Cube draggable (sans gestion de collision)

---
+++ Assets/Scripts/Objects/MovableCube.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class MovableCube : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 startPosition;
+    private Plane plane;
+
+    private void Start()
+    {
+        plane = new Plane(Vector3.up, transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            startPosition = transform.position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distanceToPlane;
+
+            if (plane.Raycast(ray, out distanceToPlane))
+            {
+                Vector3 hitPoint = ray.GetPoint(distanceToPlane);
+                hitPoint.y = startPosition.y;
+
+                transform.position = hitPoint;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/SliderZ.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class SliderZ : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 startPosition;
+    private Plane plane;
+
+    private void Start()
+    {
+        plane = new Plane(Vector3.up, transform.position);
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragging = true;
+            startPosition = transform.position;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (isDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            float distanceToPlane;
+
+            if (plane.Raycast(ray, out distanceToPlane))
+            {
+                Vector3 hitPoint = ray.GetPoint(distanceToPlane);
+                hitPoint.y = startPosition.y;
+                hitPoint.x = startPosition.x;
+
+                transform.position = hitPoint;
+            }
+        }
+    }
+}
